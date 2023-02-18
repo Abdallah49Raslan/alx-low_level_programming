fd0f01eaff1b1330fbeb63011cb8b7666ae06ae3
abdallah <abdallah49raslan@gmail.com>
2023-02-17 20:44:20 -0800

    while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,10 +6,12 @@
 /* betty style doc for function main goes there */
 /**
  * main - print greater than 5, equal 0 and less than 6 and not 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	int m;
 
 	srand(time(0));
@@ -21,6 +23,6 @@ int main(void)
 	if (m == 0)
 		printf("Last digit of %d is %d  and is 0\n", n, m);
 	if (m < 6 && m != 0)
-		printf("%d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
