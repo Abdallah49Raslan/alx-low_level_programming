c18cbc79da9693901b510f69131e8e6f851d733d
abdallah <abdallah49raslan@gmail.com>
2023-02-23 10:07:52 -0800

    finish

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0 fib2 = 1, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
@@ -27,12 +27,12 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (fib1_haf2 + fib2_half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-		printf("%lu%lu", hlaf1, half2);
+		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
 		fib1_half1 = fib2_half1;
