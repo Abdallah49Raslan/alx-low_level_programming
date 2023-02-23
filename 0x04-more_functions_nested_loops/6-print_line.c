5d3e225b7676cf7d19c971650677ba71cdff985c
abdallah <abdallah49raslan@gmail.com>
2023-02-23 11:53:56 -0800

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_line - draws a staight line in the terminal
- * @n: number of times the charcter _ should be printed
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 
 void print_line(int n)
