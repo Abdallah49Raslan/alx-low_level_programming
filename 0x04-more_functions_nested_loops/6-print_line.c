d719de42038c58cd42adb9605a5b85e741bfa5c5
abdallah <abdallah49raslan@gmail.com>
2023-02-23 11:29:26 -0800

    8

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "mian.h"
+
+/**
+ * print_line - draws a staight line in the terminal
+ * @n: number of times the charcter _ should be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('n');
+	}
+}
