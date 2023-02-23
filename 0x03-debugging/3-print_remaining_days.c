83920da04da2c55590bd57184011bb49b9931e02
abdallah <abdallah49raslan@gmail.com>
2023-02-23 09:02:13 -0800

    end

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -28,7 +28,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 363 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
