862f499fe7bc959798eb4f91ec7dc71d0b597615
abdallah <abdallah49raslan@gmail.com>
2023-02-23 16:15:22 -0800

    end

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an ineger.
+ * @n: teh ineger to prints.
+ * Rerurn: nothing!
+ */
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}
