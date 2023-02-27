040b361a8298d5cc7a5472fd0414e09b6711a532
abdallah <abdallah49raslan@gmail.com>
2023-02-27 12:31:37 -0800

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * rest_to_98 - Taskes a pointer to an int and updets
- * the value it points to 98
+ * reset_to_98 - Taskes a pointer to an int and updets
+ * the value it points to to 98
  * @n: An interger input pointer
  * Return: nothing
  */
-void rest_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void rest_to_98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
