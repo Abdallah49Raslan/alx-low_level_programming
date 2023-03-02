6d7f4210a703d4f6a1505c7d90bd2e22c5665d08
abdallah <abdallah49raslan@gmail.com>
2023-03-02 06:28:07 -0800

    8:10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, a[5], *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
